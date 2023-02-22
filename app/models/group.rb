e502cc199202f22e61cf4d0614df37869b17cf92
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2023-02-22 11:35:36 +0100

    Update Group and User Models

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,4 +6,8 @@ class Group < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :group_expenses, dependent: :destroy
   has_many :expenses, through: :group_expenses, dependent: :destroy
+
+  def total_expenses
+    expenses.sum(:amount)
+  end
 end
